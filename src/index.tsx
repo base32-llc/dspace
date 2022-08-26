83057d119c38f1a58e8b6f97263ebf66bd2c2521
ExtraHash <extrahash@protonmail.com>
2022-08-26 15:54:13 -0600

    switch to hashrouter since github does not support browsertouer

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 const root = ReactDOM.createRoot(
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <RecoilRoot>
                 <App />
             </RecoilRoot>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
 
