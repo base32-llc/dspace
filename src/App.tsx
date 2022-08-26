6b3fdacc136a4cd68536cdcbc566be45e0dc94ac
ExtraHash <extrahash@protonmail.com>
2022-08-26 16:34:19 -0600

    update profile page

--- src/App.tsx
+++ src/App.tsx
@@ -274,21 +274,31 @@ const HomePage: FC = () => {
             <div className="container mx-auto mt-6">
                 {user && (
                     <p className="text-xl">
-                        Welcome <strong>{user.username}</strong>
-                        <br />
-                        <small>
-                            <span
-                                className="font-medium text-blue-600 underline pointer"
-                                onClick={() => {
-                                    setNewUsernameOpen(true);
-                                }}
-                            >
-                                change username
-                            </span>
-                        </small>
+                        <strong>@{user.username}</strong>
+                        <svg
+                            style={{
+                                display: "inline-block",
+                                marginLeft: "2px",
+                            }}
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="text-gray-700 pointer w-5 h-5"
+                            onClick={() => {
+                                setNewUsernameOpen(true);
+                            }}
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                            />
+                        </svg>
                     </p>
                 )}
-                {!user && (
+                {!user && wallet.connected && (
                     <div className="flex">
                         <div
                             className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
@@ -299,6 +309,12 @@ const HomePage: FC = () => {
                             </span>
                         </div>
                     </div>
+                )}
+
+                {!user && !wallet.connected && (
+                    <p className="text-xl">
+                        <strong>Please connect your wallet to continue.</strong>
+                    </p>
                 )}
             </div>
         </div>
